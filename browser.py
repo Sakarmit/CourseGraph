a41bc41d8a065215cef3f3997752c0b072b58cf9
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2025-02-15 22:01:11 -0500

    browser login caching

--- browser.py
+++ browser.py
@@ -1,11 +1,15 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def create_browser_instance():
     chrome_options = Options()
 
+    uncached_folder = os.path.join(os.getcwd(), 'uncached', 'chrome-data')
+
     #chrome_options.add_argument("--headless=new")
     #chrome_options.add_argument("--auto-open-devtools-for-tabs")
+    chrome_options.add_argument(f"--user-data-dir={uncached_folder}")
     chrome_options.add_experimental_option("detach", True)
     
     return webdriver.Chrome(options=chrome_options)
--- main.py
+++ main.py
@@ -4,26 +4,4 @@ import json
 import os
 browser = create_browser_instance()
 
-try:
-    with open('uncached/cookies.json', 'r') as file:
-        cookies = json.load(file)
-        for cookie in cookies:
-            browser.add_cookie(cookie)
-            cookie.domain = 'https://webauth.uncc.edu'
-            browser.add_cookie(cookie)
-except:
-    print("No cookies to load")
-browser.refresh()
-
 load_degree_works(browser)
-cookies = browser.get_cookies()
-try:
-    os.makedirs('uncached', exist_ok=True)
-    file_path = 'uncached/cookies.json'
-    if not os.path.exists(file_path):
-        with open(file_path, 'w') as file:
-            file.write('{}')
-    with open(file_path, 'r') as file:
-        json.dump(cookies, file)
-except:
-    print("Failed to save cookies")
