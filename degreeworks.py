a803bb98ece225b0ffc7c55792804c17017df8bd
nmohame6 <135072729+nmohame6@users.noreply.github.com>
2025-02-18 15:11:30 -0500

    docs: Adding comments to degreeworks.py for documentation purposes

--- degreeworks.py
+++ degreeworks.py
@@ -1,21 +1,41 @@
+##importing seleniummodules for data scraping
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+##importing modules
 import json
 import os
 
 degree_works = 'https://degreeworks.charlotte.edu/worksheets/WEB31'
 user_id = None
 
+"""
+    load_degree_works function : Loads the Degree Works page and ensures the user is logged in.
+
+    Args:
+        browser: A selenium.webdriver instance used to interact with the
+                 web page.
+    """
 def load_degree_works(browser):
+    ## fetching degreeworks page
     browser.get(degree_works)
     if not 'https://degreeworks.charlotte.edu/worksheets/WEB31' == browser.current_url:
         log_in_user(browser)
+    ## wait for page to load
     WebDriverWait(browser, 10)
+    ## fetching degreeworks data
     get_degree_works_data(browser)
 
+""" 
+    log_in_user function : Logs in the user to the Degree Works page.
+
+    Args:
+        browser: A selenium.webdriver instance used to interact with the
+                 web page.
+"""
 def log_in_user(browser):
+    ## allow for user login
     try:
         username_field = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.ID, "username"))
@@ -48,20 +68,30 @@ def log_in_user(browser):
         print("Failed to get Degreeworks after login")
         browser.quit()
 
+"""
+    get_degree_works_data function : Fetches the data from the Degree Works page.
+
+    Args:
+        browser: A selenium.webdriver instance used to interact with the
+                 web page.
+    """
 def get_degree_works_data(browser):
+    ## finding user id
     user_id = browser.find_element(By.ID, "student-id").get_attribute("value")
-    testFetch = f'''
+    degreeDataFetch = f'''
     return fetch("https://degreeworks.charlotte.edu/api/audit?studentId={user_id}&school=UG&degree=BS&is-process-new=false&audit-type=AA&auditId=&include-inprogress=true&include-preregistered=true&aid-term=")
     .then(response => response.json())
     .then(data => data)
     .catch(error => error);
     '''
 
-    result = browser.execute_script(testFetch)
+    result = browser.execute_script(degreeDataFetch)
+    ## saving student data to json file 
     try:
         os.makedirs('uncached', exist_ok=True)
         file_path = 'uncached/file.json'
         with open(file_path, 'w+') as file:
             json.dump(result, file)
+    ## messaging signaling error in saving json file
     except:
         print("Failed to save cookies")
